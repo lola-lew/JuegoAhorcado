dd144c494a3fcfdadaa9f2e1bb36b18e44743586
Loli <lolarlew@gmail.com>
2023-10-10 19:26:17 -0300

    probando

--- Main/Main.java
+++ Main/Main.java
@@ -4,7 +4,7 @@ import Servicio.AhorcadoServicio;
 
 public class Main {
   public static void main(String[] args) {
-    
+    /*coment para push */
     AhorcadoServicio jugar = new AhorcadoServicio();
     System.out.println("\nHoli! Bienvenidos a EL AHORCADO (^o^)");
     jugar.menu();
